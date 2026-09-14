8769d907f75903a8b413ff42982866a1a7fd91bd
jycxn <007jc.chan@gmail.com>
2026-09-14 14:22:33 -0400

    for to while loop

--- src/lab1/FizzBuzz.java
+++ src/lab1/FizzBuzz.java
@@ -7,11 +7,18 @@ class FizzBuzz {
 
     public static void main(String[] args) {
 
-        for (int i = 1; i <= 100; i++) {
+        dofizzbuzz();
+    }
+
+    private static void dofizzbuzz() {
+        int i = 1;
+        while (i <= 100) {
 
             // Find out which numbers divide i.
             boolean divisibleBy3 = i % 3 == 0;
             boolean divisibleBy5 = i % 5 == 0;
+
+            i++;
 
             // Print our appropriate result.
             if (divisibleBy3 && divisibleBy5) {
